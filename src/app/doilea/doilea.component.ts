3a45cdccc31b52f77df1e0bb9c7bd03146b0a1a7
Dst2009 <dorinstoenescu.m@gmail.com>
2023-09-27 16:03:08 +0300

    mesaj

---
+++ src/app/doilea/doilea.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-doilea',
+  templateUrl: './doilea.component.html',
+  styleUrls: ['./doilea.component.css']
+})
+export class DoileaComponent {
+
+@Input() childGivenName: string = "";
+@Output() valueEmitter = new EventEmitter<string>()
+
+onClickFromChild() {
+  this.valueEmitter.emit("Salut din componenta copil")
+}
+
+
+}
